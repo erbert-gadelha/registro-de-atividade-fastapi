7c1418cb20c43a5aa84a68a426dc53c2988e140a
erbert-gadelha <ebgr@cin.ufpe.br>
2025-02-14 16:26:16 -0300

    [feat] criado model de obrigacao/acessoria

--- models.py
+++ models.py
@@ -13,3 +13,13 @@ class Empresa(Base):
     def __repr__(self) -> str:
         return f"Empresa(id={self.id!r}, nome={self.nome!r}, cnpj={self.cnpj!r}, nome={self.nome!r}, endereco={self.endereco!r}, telefone={self.telefone!r})"
 
+
+class ObrigacaoAcessoria(Base):
+    __tablename__ = "obrigacaoAcessoria_tb"
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String, index=True)
+    periodicidade = Column(String, index=True)
+    empresa = Column(Integer, ForeignKey(Empresa.id), nullable=False)
+
+    def __repr__(self) -> str:
+        return f"ObrigacaoAcessoria(id={self.id!r}, nome={self.nome}, periodicidade={self.periodicidade}, empresa={self.empresa})"
