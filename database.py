433a47acd344065633068da785f5e9964c7ab6ae
erbert-gadelha <ebgr@cin.ufpe.br>
2025-02-14 19:47:13 -0300

    [refact] credencias do banco são variáveis de ambiente

--- database.py
+++ database.py
@@ -1,10 +1,18 @@
-# database.py
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+db_host = os.getenv('DB_HOST')
+db_port = os.getenv('DB_PORT')
+db_name = os.getenv('DB_NAME')
+db_user = os.getenv('DB_USER')
+db_pw =   os.getenv('DB_PW')
 
-SQLALCHEMY_DATABASE_URL = "postgresql://user:@localhost:5432/meubanco"
+SQLALCHEMY_DATABASE_URL = f'postgresql://{db_user}:{db_pw}:@{db_host}{db_port}/{db_name}'
+#print(SQLALCHEMY_DATABASE_URL)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
