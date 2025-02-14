62077abeb3e97bd591e5f78a91989c084b137a6f
erbert-gadelha <ebgr@cin.ufpe.br>
2025-02-14 20:15:33 -0300

    [feat] rota para criacao de empresa implementada

---
+++ router.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter, Depends
+import psycopg2
+from sqlalchemy.orm import Session
+from database import get_db
+from models import Empresa
+from starlette.responses import JSONResponse
+from schemas import EmpresaDTO
+
+
+empresa = APIRouter(prefix="/empresa")
+
+@empresa.post("/")
+def criar_empresa(request:EmpresaDTO, db: Session = Depends(get_db)) -> JSONResponse:
+    db_empresa = Empresa(dto=request)
+
+    db.add(db_empresa)
+    db.commit()
+    db.refresh(db_empresa)
+
+    dto = EmpresaDTO.from_orm(db_empresa).model_dump()
+    return JSONResponse(content={
+        'detail': 'Criado com sucesso.',
+        'id': db_empresa.id,
+        'result': dto
+    }, status_code=201)
