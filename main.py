dea8ee2cef95dcfee52629319b7d21795fe99048
erbert-gadelha <ebgr@cin.ufpe.br>
2025-02-15 05:49:28 -0300

    [config] definidas tags do Swagger.

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 
+from fastapi import FastAPI
+from database import Base, engine
+import router
+
+Base.metadata.drop_all(bind=engine)
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.include_router(router.empresa, tags=["Empresas"])
+app.include_router(router.obrigacao_acessoria, tags=["Obrigacao e Acessoria"])
